3c08044c627fb5e3176160df1183f2c9aa7e908d
urmil404 <urmil8989@gmail.com>
2021-11-06 12:25:06 +0530

    time table ready

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import pywhatkit as pwt
-pwt.sendwhatmsg("+918128389164","hy bro",14,36)
-
+import pywhatkit as kit
+import timeTable as tt
+from datetime import datetime
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+current_hour = int(current_time[:2])
+current_minute = int(current_time[3:5])
 def openWP():
     print("Opening WhatsApp...")
-    pwt.search("Javascript")
+    # kit.sendwhatmsg_instantly("+918128389164","demo msgs",current_hour,current_minute+1,5)
+    kit.sendwhatmsg_to_group("GyHLAbFQwuqHnA6Rw3rlJ5", "Hy guys", current_hour, current_minute+1, 2)
+def showTodaysLectures(day):
+    for i in range(1, 5):
+        print(tt.Lecture[day][i])
 
-def showCalender():
-    pass
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    showTodaysLectures("Monday")
     openWP()
-    # showCalender()
