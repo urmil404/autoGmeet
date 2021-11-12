cee7c5d4c9e0c3310b9b66863db798df5015454f
urmil404 <urmil8989@gmail.com>
2021-11-12 13:23:47 +0530

    message sending done

--- main.py
+++ main.py
@@ -4,34 +4,36 @@ from datetime import datetime
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
+check_time = now.strftime("%H:%M")
 current_hour = int(current_time[:2])
 current_minute = int(current_time[3:5])
 msg = ""
+print(check_time)
 
 
 def showTodaysLectures(day):
     global msg
-    for i in range(1, 5):
-        if i==1:
-            time = 9.30
-            lecName = tt.Lecture[day][i]
+    while(check_time == "14:00"):
+        print("checking time...")
+        if check_time == "09:30":
+            lecName = tt.Lecture[day][1]
             msg = f"{lecName[1]}"
-        elif i==2:
-            lecName = tt.Lecture[day][i]
+            print(f"{day} Lecture 1 Started")
+        elif check_time == "10:30":
+            lecName = tt.Lecture[day][2]
             msg = f"{lecName[2]}"
-        elif i==3:
-            lecName = tt.Lecture[day][i]
+        elif check_time == "12:00":
+            lecName = tt.Lecture[day][3]
             msg = f"{lecName[3]}"
-        elif i==4:
-            lecName = tt.Lecture[day][i]
+        elif check_time == "13:00":
+            lecName = tt.Lecture[day][4]
             msg = f"{lecName[4]}"
 
-            
 
 def openWP(msg):
     print("Opening WhatsApp...")
-    print(msg)
-    # kit.sendwhatmsg_instantly("+918128389164", msg,current_hour, current_minute+1, 5)
+    print(f"Link is ===>{msg}")
+    kit.sendwhatmsg_instantly("+911234567890", msg,current_hour, current_minute+1, 5)
     # kit.sendwhatmsg_to_group("GyHLAbFQwuqHnA6Rw3rlJ5", "Hy guys", current_hour, current_minute+1, 2)
 
 
