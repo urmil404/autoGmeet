ebc886451b5be1f3a03a0b5eb86f09ee7877c5f7
urmil404 <urmil8989@gmail.com>
2021-11-12 11:02:15 +0530

    middle of project

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import pywhatkit as kit
 import timeTable as tt
 from datetime import datetime
+
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 current_hour = int(current_time[:2])
 current_minute = int(current_time[3:5])
-def openWP():
-    print("Opening WhatsApp...")
-    # kit.sendwhatmsg_instantly("+918128389164","demo msgs",current_hour,current_minute+1,5)
-    kit.sendwhatmsg_to_group("GyHLAbFQwuqHnA6Rw3rlJ5", "Hy guys", current_hour, current_minute+1, 2)
+msg = ""
+
+
 def showTodaysLectures(day):
     for i in range(1, 5):
-        print(tt.Lecture[day][i])
+        lecName = tt.Lecture[day][i]
+        global msg
+        msg = f"{lecName['1-OSTP - KG']}"
+        # msg = f"{lecName}]"
+        break
+
+def openWP(msg):
+    print("Opening WhatsApp...")
+    print(msg)
+    # kit.sendwhatmsg_instantly("+918128389164", msg,current_hour, current_minute+1, 5)
+    # kit.sendwhatmsg_to_group("GyHLAbFQwuqHnA6Rw3rlJ5", "Hy guys", current_hour, current_minute+1, 2)
 
 
 if __name__ == "__main__":
-    showTodaysLectures("Monday")
-    openWP()
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    showTodaysLectures(days[0])
+    openWP(msg)
+    # for d in days:
